216e332f880880db8b57bef8028ae6f76c29aad1
UrocyonF <95148544+UrocyonF@users.noreply.github.com>
2023-10-22 22:06:03 +0200

    Update cpe3d.py

--- cpe3d.py
+++ cpe3d.py
@@ -1,3 +1,13 @@
+"""
+Copyright (c) 2023, UrocyonF
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree. 
+
+Author: UrocyonF
+Date: 2022 - 2023
+"""
 import OpenGL.GL as GL
 import numpy as np 
 import pyrr
